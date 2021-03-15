333855b89b0268d8e0f373f5e738d67e26fc6169
SD6399 <david-shapiro@mail.ru>
2021-03-15 13:04:11 +0400

    Доделано контрастирование. Теперь 2 графика на экране

--- lab2.py
+++ lab2.py
@@ -11,21 +11,30 @@ gmax=255
 gmin=0
 
 arr = np.array(skimage.io.imread("D:/dk/university/mpai/lab_2/imgg/07_elaine.tif"))
-arr1 = np.array(skimage.io.imread("D:/dk/university/mpai/lab_2/imgg/01_apc.tif"))
+arr1 = np.array(skimage.io.imread("D:/dk/university/mpai/lab_2/imgg/01_apc.tif"), dtype='uint8')
 
-b = np.array([])
-print(type(arr))
-print(arr)
+bb = np.array([])
 
 arr[arr < 127] = 0
 arr[arr > 127] = 255
 
 fmax=np.amax(arr1)
 fmin=np.amin(arr1)
+
+
 a=(gmax-gmin)/(fmax-fmin)
 b=(gmin*fmax-gmax*fmin)/(fmax-fmin)
 print(a,b)
 
-imshow(arr)
+print(arr1)
+bb=(arr1*a+b).astype(np.uint8)
+print(bb)
+
+plt.subplot(2,2,1)
+imshow(arr1)
+plt.title("До",size=10)
+plt.subplot(2,2,2)
+imshow(bb)
+plt.title("После",size=10)
 plt.show()
 # show()
